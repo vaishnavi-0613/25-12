f688e7b63ae54ed99ca90b8241eed6f018c5c97b
vaishnavi-0613 <vaishnavibeenaveni@gmail.com>
2025-12-24 19:19:39 +0530

    Create Login.js

---
+++ pages/pages/Login.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (email === "admin@gmail.com" && password === "admin@123") {
+      localStorage.setItem("isLoggedIn", true);
+      navigate("/todos");
+    } else {
+      setError("Invalid email or password");
+    }
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
